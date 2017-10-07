c4d9950b10df92021737409276009b2ed4878b19
corecipher <sarora@mphrx.com>
2017-10-07 23:49:13 +0530

    Changing the underlying framework

--- proxy_server/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class ProxyServerConfig(AppConfig):
-    name = 'proxy_server'
--- proxy_server/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import requests
-
-# Create your views here.
-def func(req, url):
-    resp = requests.get(url)
-    return HttpResponse(resp)
